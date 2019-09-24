c166a0a7e7cd02192a225964f22d383ce786db5b
_Priyank <priyank.amplify@gmail.com>
2019-09-25 00:23:42 +0530

    kind of types

---
+++ fetchJSON/exampleTypes.ts
@@ -0,0 +1,15 @@
+/**
+ * hover on the variables to see their types
+ */
+
+const Dte = new Date();
+const fn = function(kim, kar) {};
+const fn_ = function(kim: number, kar: string): number {
+  return 10;
+};
+const obj = {
+  age: 10,
+  name: "test"
+};
+class Color {}
+const green = new Color();
